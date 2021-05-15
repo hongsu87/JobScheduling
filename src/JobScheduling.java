8d57dab5ea7e02bbe23ae0485baa2f1cfede74e0
hongsu87 <hongsubakgame@naver.com>
2021-05-15 18:25:39 +0900

    .

---
+++ src/JobScheduling.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+
+public class JobScheduling {
+    public static void main(String[] args) {
+        int n = 4; // n : 작업의 개수
+        int m = 2; // m : 기계의 개수
+        int[] t = new int[n]; // t : 각 작업의 수행 시간
+        Random random = new Random();
+        System.out.print("작업 시간 : ");
+        for (int i = 0; i < n; i++) {
+            t[i] = random.nextInt(10) + 1;
+            System.out.printf("%d ", t[i]);
+        }
+        System.out.println();
+        System.out.println(schedule(n, m, t));
+    }
+
+    public static int schedule(int n, int m, int[] t) {
+        int[] L = new int[m]; // L : 각 기계의 마지막 작업의 종료 시간
+        // 초기화
+        for (int j = 0; j < m; j++) {
+            L[j] = 0;
+        }
+        // 제일 먼저 끝나는 기계에 작업 배정
+        for (int i = 0; i < n; i++) {
+            int min = 0;
+            for (int j = 1; j < m; j++) {
+                if (L[j] < L[min]) {
+                    min = j;
+                }
+            }
+            L[min] = L[min] + t[i];
+        }
+        // 가장 마지막으로 종료되는 시간 찾기
+        int max = L[0];
+        for (int i = 1; i < m; i++) {
+            if (L[i] > max) {
+                max = L[i];
+            }
+        }
+        return max;
+    }
+}
